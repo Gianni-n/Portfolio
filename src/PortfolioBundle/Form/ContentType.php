5fab6c839e3f0f46763dc044bb8574e600acceab
gianni-n <gl.napoli82@gmail.com>
2017-03-17 23:28:32 +0100

    routing, crud and pages created

---
+++ src/PortfolioBundle/Form/ContentType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PortfolioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PortfolioBundle\Entity\Content'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'portfoliobundle_content';
+    }
+
+
+}
