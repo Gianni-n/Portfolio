5c6785f33dee719a4280402c01c7ad96c11b49d2
gianni-n <gl.napoli82@gmail.com>
2017-03-25 18:19:26 +0100

    admin section with role_admin

--- src/PortfolioBundle/Controller/DefaultController.php
+++ src/PortfolioBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -31,5 +32,21 @@ class DefaultController extends Controller
     public function contactAction()
     {
         return $this->render('PortfolioBundle:Default:contact.html.twig');
+    }
+
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 }
